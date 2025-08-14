9d73cc43ddca82154bc15e2ec62a332d2ba6572f
devlitus <developercarles@gmail.com>
2025-08-14 17:01:35 +0200

    fix: Corregir formato del contenedor principal y limpiar el CSS eliminando espacios innecesarios

--- src/App.tsx
+++ src/App.tsx
@@ -30,7 +30,7 @@ function App() {
   }
 
   return (
-    <div className="app">
+    <div className="app" >
       <Header darkMode={darkMode} onToggleTheme={toggleTheme} />
 
       <main className="app-main">
